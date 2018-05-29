d6bc3e4a509a552d447a3200dda60838f66323f8
Grace Brackman <gbrackma@purdue.edu>
2018-05-29 11:03:28 -0400

    add githib component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import './App.css';
-import {Route, Switch, NavLink} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Route, Switch, NavLink } from 'react-router-dom'
+
+import './App.css'
+import Github from './Github'
 
 class App extends Component {
   render() {
@@ -10,25 +12,17 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </div>
-        <ul className='navLink'>
+        <ul className="navLinks">
           <li>
-            <NavLink to="/github"> Github API </NavLink>
-          </li>
-          <li>
-            <NavLink to="/nasa"> NASA API </NavLink>
-          </li>
-          <li>
-            <NavLink to="/homework"> Homework </NavLink>
+            <NavLink to="/github">GitHub API</NavLink>
           </li>
         </ul>
         <Switch>
-          <Route path="/github" render={() => <h1>Githib!</h1>}/>
-          <Route path="/nasa" render={() => <h1>Nasa!</h1>}/>
-          <Route path="/homework" render={() => <h1>Homework!</h1>}/>
-          <Route render={() => <h1>click a link to get started!</h1>}/>
+          <Route path="/github" component={Github} />
+          <Route render={() => <p>To get started, click one of the links above.</p>} />
         </Switch>
       </div>
     )
   }
 }
-export default App;
+export default App
